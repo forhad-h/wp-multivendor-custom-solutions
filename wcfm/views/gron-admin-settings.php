4b2a743b173787980bc675cce0789025d42e5d07
forhad-h <hosainf1@gmail.com>
2021-12-13 14:54:11 +0600

    Display store list to allow/disallow specific vendors for delivery management

--- wcfm/views/gron-admin-settings.php
+++ wcfm/views/gron-admin-settings.php
@@ -32,14 +32,14 @@ $mysql = new MySQL();
       <div class="wcfm-tabWrap gron_tab_wrap">
 
         <!-- collapsible -->
-        <div class="page_collapsible" id="gron-admin-settings-general">
+        <div class="page_collapsible" id="gron-admin-settings-delivery">
           <label class="wcfmfa fa-business-time"></label>
-          <?php _e('General', 'gron-custom'); ?><span></span>
+          <?php _e('Delivery', 'gron-custom'); ?><span></span>
         </div>
 
         <div class="wcfm-container">
-          <div id="gron-admin-settings-general" class="wcfm-content">
-            <h2><?php _e('General Settings', 'gron-custom'); ?></h2>
+          <div id="gron-admin-settings-delivery" class="wcfm-content">
+            <h2><?php _e('Delivery Settings', 'gron-custom'); ?></h2>
             <div class="wcfm_clearfix"></div>
             <form
               id="gron-general-settings-form"
@@ -88,11 +88,60 @@ $mysql = new MySQL();
                 ?>
 
                 <p class="gron_field_title"><strong>Delivery notification broadcast time limit</strong></p>
-                <label class="screen-reader-text" for="store_ppp">Delivery notification broadcast time limit</label>
+                <label class="screen-reader-text">Delivery notification broadcast time limit</label>
 
                 <div class="field">
                   <input type="number" id="time-limit" class="wcfm-text" value="<?php echo $time_limit;?>">
                   <p class="field_desc">Provide the time limit as <strong>Minute</strong>.</p>
+                </div>
+
+              </div>
+              <!-- each field -->
+
+              <!-- each field -->
+              <div class="each_field gron_multiple_checkbox_fields">
+
+                <?php
+                  $vendor_role = 'wcfm_vendor';
+                  $args = array(
+                    'role__in'     => array( $vendor_role ),
+                    'orderby'      => 'ID',
+                    'order'        => 'ASC',
+                    'fields'       => "ID"
+                   );
+
+                  $vendor_ids = get_users( $args );
+
+                  $store_names = array();
+
+                  foreach( $vendor_ids as $vendor_id ) {
+                    $store_name = get_user_meta( $vendor_id, 'store_name', true );
+                    $store_names[$vendor_id] = $store_name;
+                  }
+
+                ?>
+
+                <p class="gron_field_title"><strong>Vendor Deliveries Allowed by Admin</strong></p>
+                <label class="screen-reader-text">Vendor Deliveries Allowed by Admin</label>
+
+                <div class="fields">
+
+                  <?php foreach( $store_names as $vendor_id => $store_name): ?>
+
+                    <div class="each_field">
+
+
+
+                        <input type="checkbox" id="gron-store-<?php echo $vendor_id; ?>" class="wcfm-checkbox" value="<?php echo $vendor_id;?>" />
+
+                        <label for="gron-store-<?php echo $vendor_id; ?>"><?php echo $store_name; ?></label>
+
+
+
+                    </div>
+
+                  <?php endforeach;?>
+
                 </div>
 
               </div>
